5f33ba15d1a1405e40b5dd6d96c57dd479fb72b2
Muhammad Wajahat Asif <74649068+technicalwajahat@users.noreply.github.com>
2023-12-12 19:29:37 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,43 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+// Route::get('/', function () {
+//     return view('home');
+// })->name('home');
 
-Route::get('/post', function () {
-    return view('post');
-})->name('myPost');
+// Route::get('/post', function () {
+//     return view('post');
+// })->name('myPost');
 
-Route::get('/test', function () {
-    return view('about');
-})->name('about');
+// Route::get('/test', function () {
+//     return view('about');
+// })->name('about');
 
-Route::redirect('/about', '/test', 301);
- 
+
+// Route Prefix 
+Route::prefix('page')->group(function() {
+    Route::get('/home', function () {
+        return view('home');
+    })->name('home');
+    
+    Route::get('/post', function () {
+        return view('post');
+    })->name('myPost');
+    
+    Route::get('/test', function () {
+        return view('about');
+    })->name('about');
+
+    Route::redirect('/page/about', '/page/test', 301);
+});
+
+
+Route::fallback(function() {
+    return "<h1>Page Not Found!</h1>";
+});
+
+
 // 1st Method
-
 // Route::get('/post/{id?}/', function (string $id = null) {
 //     if ($id) {
 //         return "<h1>Post ID : ". $id ."</h1>";
@@ -25,6 +46,7 @@ Route::redirect('/about', '/test', 301);
 //         return "<h1>No ID Found!</h1>";
 //     }    
 // })->where('id', '[0-9]+');
+
 
 // Route::get('/post/{id?}/comment/{commentid?}', function (string $id = null, string $comment = null) {
 //     if ($id) {
@@ -34,12 +56,12 @@ Route::redirect('/about', '/test', 301);
 //     }    
 // })->where('id', '[0-9]+')->whereAlpha('commentid');
 
-// 2nd Method
 
+// 2nd Method
 // Route::get('/post', function () {
 //     return '<h1>King Waji Awan</h1>';
 // });
 
-// 3rd Method
 
+// 3rd Method
 // Route::view('/hello', 'post');

3f8f2342d2d270fb29d80f13c4ff6fd95be0e7b2
Muhammad Wajahat Asif <74649068+technicalwajahat@users.noreply.github.com>
2023-12-14 19:37:03 +0500

    Update Views

---
+++ database/migrations/2023_12_14_130434_create_libraries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libraries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->string('book');
+            $table->date('due_date')->nullable();
+            $table->boolean('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libraries');
+    }
+};

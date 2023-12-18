877778d880c00e1a8eaaa017226991b95835e932
Muhammad Wajahat Asif <74649068+technicalwajahat@users.noreply.github.com>
2023-12-18 20:08:35 +0500

    Update Migrations

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\student;
+use Illuminate\Support\Facades\File;
 
 class StudentSeeder extends Seeder
 {
@@ -15,10 +16,45 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        student::create([
-            'name' => 'Wajahat Awan',
-            'email' => 'waji@gmail.com'
-        ]);
+        // Fake Data
+        for($i = 1; $i <= 10; $i++) {
+            student::create([
+                'name' => fake()->name(),
+                'email' => fake()->unique()->email()
+            ]);
+        }      
+
+        // $json = File::get(path:'database/json/students.json');
+        // $students = collect(json_decode($json));        
+
+        // $students->each(function($student){
+        //     student::create([
+        //         'name' => $student->name,
+        //         'email' => $student->email
+        //     ]);
+        // });
+        
+
+        // $students = collect(
+        //     [
+        //         [
+        //             'name' => 'Wajahat Awan',
+        //             'email' => 'waji@gmail.com'
+        //         ],
+        //         [
+        //             'name' => 'Daniyal Afzal',
+        //             'email' => 'dani@gmail.com'
+        //         ],
+        //         [
+        //             'name' => 'Danish Bashir',
+        //             'email' => 'danish@gmail.com'
+        //         ]
+        //     ]
+        // );
+        
+        // $students->each(function($student){
+        //     student::insert($student);
+        // });    
     }
 }
  

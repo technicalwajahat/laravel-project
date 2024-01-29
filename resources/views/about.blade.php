cc2f47ce0cbc258be91b6e4bbe07713c0c5b0d68
Muhammad Wajahat Asif <74649068+technicalwajahat@users.noreply.github.com>
2024-01-29 13:23:50 +0500

    update

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.masterlayout')
+
+@section('content')
+    <h2>About Page</h2>
+@endsection
+
+@section('title')
+    About
+@endsection
---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.masterlayout')
+
+@section('content')
+    <h2>Post Page</h2>
+@endsection
+
+{{-- @section('title')
+    Post
+@endsection --}}
